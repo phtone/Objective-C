c823de5ad1f7e082e94095697606ced0faa13892
phtone <phtone@gmail.com>
2014-08-22 00:14:11 +0400

    adjustment conditions

--- BMIТime/BMIТime/main.m
+++ BMIТime/BMIТime/main.m
@@ -11,27 +11,28 @@ int main(int argc, const char * argv[]) {
         [appdelegate setWeightInKilos:96];
         [appdelegate setHeightInMeters:1.8];
         
+        
         float bmi = [appdelegate bodyMassIndex];
 
-            if (bmi == 0 - 16) {
+            if (bmi >= 0 && bmi <= 16) {
                 NSLog(@"Выраженный дефицит массы тела");
             }
-            if (bmi == 16 - 18.5) {
+            if (bmi >= 16 && bmi <= 18.5) {
                 NSLog(@"Недостаточная (дефицит) масса тела");
             }
-            if (bmi == 18.5 - 25) {
+            if (bmi >= 18.5 && bmi <= 25) {
                 NSLog(@"Норма");
             }
-            if (bmi == 25 - 30) {
+            if (bmi >= 25 && bmi <= 30) {
                 NSLog(@"Избыточная масса тела (предожирение)");
             }
-            if (bmi == 30 - 35) {
+            if (bmi >= 30 && bmi <= 35) {
                 NSLog(@"Ожирение первой степени");
             }
-            if (bmi == 35 - 40) {
+            if (bmi >= 35 && bmi <= 40) {
                 NSLog(@"Ожирение второй степени");
             }
-            if (bmi == 40 - 100) {
+            if (bmi >= 40 && bmi <= 100) {
                 NSLog(@"Ожирение третьей степени (морбидное)");
             }
         }
