554eb640133128f35dcdd8eeebca32342d7d70b3
phtone <phtone@gmail.com>
2014-09-05 00:00:24 +0400

    ARC

---
+++ BMIТime/Asset.h
@@ -0,0 +1,13 @@
+//
+//  Asset.h
+//  BMIТime
+//
+//  Created by phtone on 03.09.14.
+//  Copyright (c) 2014 phtone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Asset : NSObject
+
+@end
---
+++ BMIТime/Asset.m
@@ -0,0 +1,13 @@
+//
+//  Asset.m
+//  BMIТime
+//
+//  Created by phtone on 03.09.14.
+//  Copyright (c) 2014 phtone. All rights reserved.
+//
+
+#import "Asset.h"
+
+@implementation Asset
+
+@end
